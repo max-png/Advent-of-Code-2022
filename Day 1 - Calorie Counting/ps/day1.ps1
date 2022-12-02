593fb4055ccb67e077356fa22471fe20938d18cf
Max Rune <max.rune@gmail.com>
2022-12-02 16:19:47 +0100

    lycka!

--- Day 1 - Calorie Counting/ps/day1.ps1
+++ Day 1 - Calorie Counting/ps/day1.ps1
@@ -1,10 +1,15 @@
-$lines = Get-Content -Path "C:\node\aoc22\Day 1 - Calorie Counting\input.txt" -Head 20
+$lines = Get-Content -Path "C:\utveckling\aoc22\Advent-of-Code-2022\Day 1 - Calorie Counting\input.txt"
+$sum = 0
+$maxsum = 0
 
-foreach ($line in $lines)
-{
-    if($line -eq "")
-    {
-        Write-Host "tomt"
+foreach ($line in $lines) {
+    $sum += $line
+    if ($line -eq "") {  
+        $currentsum = $sum
+        $sum = 0
+        if ($maxsum -lt $currentsum) {
+            $maxsum = $currentsum
+        }
     }
-    Write-Host $line
 }
+Write-Host $maxsum

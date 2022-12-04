5c6fd7c518cc89335c6121a020ec64ab248524ab
maxrune <max.rune@bedege.se>
2022-12-04 14:57:40 +0100

    day 2 p2

--- Day 2 - Rock Paper Scissors/d22.ps1
+++ Day 2 - Rock Paper Scissors/d22.ps1
@@ -4,6 +4,8 @@ Paper B/Y = 2
 Scissors C/Z = 3
 Loss = 0, Draw = 3, Win = 6 #>
 #>
+
+# Har bara räknat ut poängen själv
 $scores = @{
     "A" = @{"X" = 4; "Y" = 8; "Z" = 3 }
     "B" = @{"X" = 1; "Y" = 5; "Z" = 9 }
@@ -14,3 +16,23 @@ Get-Content "Day 2 - Rock Paper Scissors\input.txt" | ForEach-Object {
     $score += $scores[[string]$_[0]][[string]$_[2]]
 }
 Write-Host $score
+
+#### Del 2
+
+<#
+    X = förlust 0
+    Y = oavgjort 3
+    Z = vinst 6
+#>
+
+$scores2 = @{
+    "A" = @{"X" = 3; "Y" = 4; "Z" = 8 }
+    "B" = @{"X" = 1; "Y" = 5; "Z" = 9 }
+    "C" = @{"X" = 2; "Y" = 6; "Z" = 7 }
+}
+
+$score2 = 0
+Get-Content "Day 2 - Rock Paper Scissors\input.txt" | ForEach-Object {
+    $score2 += $scores2[[string]$_[0]][[string]$_[2]]
+}
+Write-Host $score2

9578b1ba9fb03319281ef8bc5055978564cdce6d
maxrune <max.rune@bedege.se>
2022-12-04 07:19:06 +0100

    d2

---
+++ Day 2 - Rock Paper Scissors/d2.ps1
@@ -0,0 +1,11 @@
+<# Parse the strategy guide to determine what your opponent will play in each round and what you should play in response.
+For each round, calculate the score for the shape you selected based on the following: Rock = 1, Paper = 2, Scissors = 3.
+Determine the outcome of the round and add the corresponding score to your total score: Loss = 0, Draw = 3, Win = 6.
+Repeat this process for each round until you have gone through the entire strategy guide.
+Add up all the scores for each round to get your total score. #>
+
+<# Parse the strategy guide to determine that in the first round your opponent will play Rock (A) and you should play Paper (Y), in the second round your opponent will play Paper (B) and you should play Rock (X), and in the third round your opponent will play Scissors (C) and you should play Scissors (Z).
+For the first round, your score would be 2 because you selected Paper (Y) and won the round. For the second round, your score would be 1 because you selected Rock (X) and lost the round. For the third round, your score would be 3 because you selected Scissors (Z) and the round was a draw.
+Add the corresponding scores for the outcome of each round to your total score: 0 for the loss in the second round, 6 for the win in the first round, and 3 for the draw in the third round. This gives you a total score of 9 (0 + 6 + 3).
+Repeat this process for each round until you have gone through the entire strategy guide.
+Add up all the scores for each round to get your total score, which in this example would be 15 (8 + 1 + 6). #>

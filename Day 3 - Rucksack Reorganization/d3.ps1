2de09583be352598c74475e346f85b35a449107a
maxrune <max.rune@bedege.se>
2022-12-07 17:23:05 +0100

    poängjämför

--- Day 3 - Rucksack Reorganization/d3.ps1
+++ Day 3 - Rucksack Reorganization/d3.ps1
@@ -1,31 +1,34 @@
 Get-Content "Day 3 - Rucksack Reorganization\sample.txt" | ForEach-Object {
-    # $SplitContent = $_.Split()
+    $TotalItemPrio = 0
     $CharArray = $_.ToCharArray()
-    
     $Length = $_.Length
     $Half = ($_.Length / 2)
-    $charAtHalf = ($_[$Half])
-
     $com1 = $CharArray[0..($Half - 1)]
     $com2 = $CharArray[$Half..$Length]
 
+    
     $com1 | ForEach-Object {
         if ($com2 -CContains $_) {
-            Write-Host "`$com2 contains the item [$_]"
+            # TODO: Fixa en regex som jämför hela $com1 och $com2 istället för en char i taget
+            Write-Host "`$com2 matches the item [$_]"
+            $itemprio = GetItemPrio $_
+            $TotalItemPrio += $itemprio
         }
-        
     }
+    Write-Host $TotalItemPrio
 }
 
-<#
-J p p M D c J P
-c Q b q G q F b
-Z S t g n H t s S j
-G B h q F b B m s m
-d j z z w g H H g g d n f w j t
-M P D P M G p P l N f p L D l l
-d R C t w t l C S t t P t l N P N t g v P
-r D q m B s j G S p j B B s J s q q m r p
-Z h W n Z h z M M f n W W T
-D z B r m s m j s B c c J B
-#>
+function GetItemPrio {
+    param(
+        [char]$character
+    )
+    if ($character -ge "a" -and $character -le "z") {
+        return ([int]($character - ("a" -as [char])) + 1)
+    }
+    elseif ($character -ge "A" -and $character -le "Z") {
+        return ([int]($character - ("A" -as [char])) + 27)
+    }
+    else {
+        throw "Invalid character"
+    }
+}
